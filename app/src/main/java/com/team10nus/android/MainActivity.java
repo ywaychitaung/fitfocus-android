e61e72b97672302e97368b4f23a006b1d24615f0
ywaychitaung <ywaychitaung@gmail.com>
2024-01-27 00:07:42 +0800

    Init

---
+++ app/src/main/java/com/team10nus/android/MainActivity.java
@@ -0,0 +1,44 @@
+package com.team10nus.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.os.Bundle;
+import com.team10nus.android.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new DashboardFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.dashboard) {
+                replaceFragment(new DashboardFragment());
+            } else if (item.getItemId() == R.id.food) {
+                replaceFragment(new FoodFragment());
+            } else if (item.getItemId() == R.id.exercise) {
+                replaceFragment(new ExerciseFragment());
+            } else if (item.getItemId() == R.id.settings) {
+                replaceFragment(new SettingsFragment());
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
